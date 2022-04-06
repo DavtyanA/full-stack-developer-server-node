3604ca3a93981df4df642ae1843977963febc0a1
Andre Davtyan <Davtyan.a@husky.neu.edu>
2022-04-06 13:18:02 -0400

    first commit

---
+++ controllers/tuits.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        "_id": "123",
+        "postedBy": {"username": "NASA"},
+        "tuit": "In 2021, our @NASAPersevere Mars rover landed",
+        "likes": 345,
+        "comments": 228,
+        "retuits": 1488
+    },
+    {
+        "_id": "234",
+        "postedBy": { "username": "NASA" },
+        "tuit": "Ingenuity helicopter takes flight.",
+        "likes": 346785,
+        "comments": 228,
+        "retuits": 1488
+    }
+];
